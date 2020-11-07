cf0d99eda159f69bd5e222c7aaeb8100ccdf896e
Max <maxrosenb@reed.edu>
2020-11-06 17:12:55 -0800

    Added comments

--- tlb.c
+++ tlb.c
@@ -5,26 +5,32 @@
 int main(int argc, char* argv[]) {
 
     const int PAGESIZE = 8192;
-    const long NUMPAGES = atoi(argv[1]);
+    const long NUM_PAGES = atoi(argv[1]);
     const long NUM_TRIALS = atoi(argv[2]);
 
-    char *a = malloc ((long) PAGESIZE * NUMPAGES);
+    char *a = malloc ((long) PAGESIZE * NUM_PAGES);
 
-    for (int i = 0; i < NUMPAGES; i++) {
+
+    // Allocation
+    for (int i = 0; i < NUM_PAGES; i++) {
         long jump = (long) i * PAGESIZE;
+        // Allocate each page
         a[jump] += 1;
     }
-        
+
+    // Need something to use each reference to each page for    
     long total = 0;
 
+    // Initialize start and stop times
     struct timeval c_start;
     struct timeval c_stop;
 
     gettimeofday(&c_start, 0);
 
     for (int i = 0; i < NUM_TRIALS; i++) {
+        // Empty asm is to prevent any compiler tricks
         asm("");
-        for (int j = 0; j < NUMPAGES; j++) {
+        for (int j = 0; j < NUM_PAGES; j++) {
             asm("");
                 long jump = (long) j * PAGESIZE;
                 total += a[jump];
@@ -38,14 +44,13 @@ int main(int argc, char* argv[]) {
     
     double total_time;
     total_time = ((double) (time_stop - time_start));
-    
-    double time_per_access = total_time / (NUMPAGES);
 
-    printf("%ld\t %f\t \n", NUMPAGES, time_per_access);
-    
+    double time_per_access = total_time / (NUM_PAGES);
 
+    printf("%ld\t %f\t \n", NUM_PAGES, time_per_access);
+    
     free(a);
-
+    
     return 0;
 
 }
